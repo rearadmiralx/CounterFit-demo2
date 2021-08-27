b328a77e8dabf1d8622ad750dfbb7a29b82abee5
killiankraus <44647842+killiankraus@users.noreply.github.com>
2021-08-27 10:37:50 +0800

    temp with server using mqtt using virtual sensors

---
+++ Farm/predict-plant-growth/temperature/temperature.py
@@ -0,0 +1,48 @@
+# from counterfit_connection import CounterFitConnection
+# CounterFitConnection.init('127.0.0.1', 5000)
+
+# import time
+# from counterfit_shims_seeed_python_dht import DHT
+# # You need to import this counterfit_shims_seeed_python_dht
+
+
+# sensor = DHT("11", 5)
+
+# while True:
+#     _, temp = sensor.read()
+#     print(f'Temperature {temp}°C')
+
+#     time.sleep(10)
+
+from counterfit_connection import CounterFitConnection
+CounterFitConnection.init('127.0.0.1', 5000)
+
+
+import time
+from counterfit_shims_seeed_python_dht import DHT
+import paho.mqtt.client as mqtt
+import json
+
+sensor = DHT("11", 5)
+
+id = '9f4189cc-05b2-11ec-9a03-0242ac130003'
+
+client_telemetry_topic = id + '/telemetry'
+client_name = id + 'temperature_sensor_client'
+
+mqtt_client = mqtt.Client(client_name)
+mqtt_client.connect('test.mosquitto.org')
+
+mqtt_client.loop_start()
+
+print("MQTT connected!")
+
+while True:
+    _, temp = sensor.read()
+    telemetry = json.dumps({'temperature' : temp})
+
+    print("Sending telemetry ", telemetry)
+
+    mqtt_client.publish(client_telemetry_topic, telemetry)
+
+    time.sleep(5)
